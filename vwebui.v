e64a4b786ef8ad24195e59399fd024f7189ff76d
Mehmet Ali <mehmetalisipi@gmail.com>
2023-04-24 15:31:38 +0300

    Remove webui_core code from library

--- vwebui.v
+++ vwebui.v
@@ -7,14 +7,16 @@ module vwebui
 #flag windows -lws2_32
 #flag windows -luser32
 
+// Consts
+
 pub const (
+	event_disconnected = 0
 	event_connected = 1
 	event_multi_connection = 2
 	event_unwanted_connection = 3
-	event_disconnected = 4
-	event_mouse_click = 5
-	event_navigation = 6
-	event_callback = 7
+	event_mouse_click = 4
+	event_navigation = 5
+	event_callback = 6
 	browser_any = 0
 	browser_chrome = 1
 	browser_firefox = 2
@@ -26,35 +28,15 @@ pub const (
 	browser_vivaldi = 8
 	browser_epic = 9
 	browser_yandex = 10
-	browser_custom = 99
 )
 
-// C Functions & Typedefs
-
-pub struct C.webui_window_core_t {
-pub mut:
-	window_number        u32
-	server_running       bool
-	connected            bool
-	server_handled       bool
-	multi_access         bool
-	server_root          bool
-	server_port          u32
-	url                  &char
-	html                 &char
-	html_cpy             &char
-	icon                 &char
-	icon_type            &char
-	CurrentBrowser       u32
-	browser_path         &char
-	profile_path         &char
-	connections          u32
-	runtime              u32
-	detect_process_close bool
-	has_events           bool
-}
+// Typedefs of struct
 
 pub type Window_t = voidptr
+pub type Webui_function = fn(details &C.webui_event_t)
+pub type Event_t = C.webui_event_t
+
+// C Functions & Typedefs
 
 pub struct C.webui_event_t {
 pub mut:
@@ -74,75 +56,13 @@ pub mut:
 	data   &char
 }
 
-pub struct C.webui_script_t {
-pub mut:
-	script  &char
-	timeout u32
-	result  C.webui_javascript_result_t
-}
-
-pub struct C.webui_cb_t {
-pub mut:
-	win               Window_t
-	webui_internal_id &char
-	element_name      &char
-	data              voidptr
-	data_len          u32
-	event_type        int
-}
-
-pub struct C.webui_cmd_async_t {
-pub mut:
-	win Window_t
-	cmd &char
-}
-
-pub struct C.webui_custom_browser_t {
-pub mut:
-	app       &char
-	arg       &char
-	auto_link bool
-}
-
-pub struct C.webui_runtime_t {
-pub mut:
-	none_  u32
-	deno   u32
-	nodejs u32
-}
-
-pub struct C.webui_t {
-pub mut:
-	servers                u32
-	connections            u32
-	process                u32
-	custom_browser         &C.webui_custom_browser_t
-	wait_for_socket_window bool
-	html_elements          [1024]&char
-	used_ports             [1024]u32
-	last_window            u32
-	startup_timeout        u32
-	use_timeout            bool
-	timeout_extra          bool
-	exit_now               bool
-	run_responses          [1024]&char
-	run_done               [1024]bool
-	run_error              [1024]bool
-	run_last_id            u32
-	browser                C.webui_browser_t
-	runtime                C.webui_runtime_t
-	initialized            bool
-	executable_path &char
-	ptr_list        [1024]voidptr
-	ptr_position    u32
-	ptr_size        [1024]usize
-}
-
 fn C.webui_new_window() Window_t
 
 fn C.webui_bind(win Window_t, element &char, func fn (&C.webui_event_t)) u32
 
 fn C.webui_show(win Window_t, content &char) bool
+
+fn C.webui_show_browser(win Window_t, url &char, browser u32) bool
 
 fn C.webui_wait()
 
@@ -150,7 +70,19 @@ fn C.webui_close(win Window_t)
 
 fn C.webui_exit()
 
+fn C.webui_is_shown(win Window_t) bool
+
+fn C.webui_set_timeout(second u32)
+
+fn C.webui_set_icon(win Window_t, icon_s &char, type_s &char)
+
+fn C.webui_multi_access(win Window_t, status bool)
+
+// ? fn C.webui_run(win Window_t, script &char)
+
 fn C.webui_script(win Window_t, script &char, timeout int, buffer &char, size_buffer int)
+
+// fn C.webui_set_runtime(win Window_t, script &char)
 
 fn C.webui_get_int(e &C.webui_event_t) i64
 
@@ -164,30 +96,15 @@ fn C.webui_return_string(e &C.webui_event_t, s &char)
 
 fn C.webui_return_bool(e &C.webui_event_t, b bool)
 
-fn C.webui_new_server(win Window_t, path &char) &char
+fn C.webui_bind_interface(win Window_t, element &char, func fn (u32, u32, &char, Window_t, &char, &&char)) u32
 
-fn C.webui_show_browser(win Window_t, url &char, browser u32) bool
-
-fn C.webui_is_any_window_running() bool
+// ? fn C.webui_interface_set_response(ptr &char, response &char)
 
 fn C.webui_is_app_running() bool
 
-fn C.webui_is_shown(win Window_t) bool
-
-fn C.webui_set_timeout(second u32)
-
-fn C.webui_set_icon(win Window_t, icon_s &char, type_s &char)
-
-fn C.webui_multi_access(win Window_t, status bool)
-
-fn C.webui_clean_mem(p voidptr)
-
-fn C.webui_bind_interface(win Window_t, element &char, func fn (u32, u32, &char, Window_t, &char, &&char)) u32
+fn C.webui_interface_get_window_id(win Window_t) u32
 
 // V Interface
-
-pub type Webui_function = fn(details &C.webui_event_t)
-pub type Event_t = C.webui_event_t
 
 pub fn (window Window_t) script (javascript string, timeout int, size_buffer int) string {
 	response := &char(" ".repeat(size_buffer).str)
@@ -198,7 +115,7 @@ pub fn (window Window_t) script (javascript string, timeout int, size_buffer int
 struct WebuiResponseData {
 pub mut:
 	string	string
-	int	int
+	int		int
 	bool	bool
 }
 
