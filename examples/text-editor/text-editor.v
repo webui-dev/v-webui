d88798ab7f1d08b062f018c69205a0d0a17d5a8d
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2023-05-09 23:33:46 +0300

    Finish V port

--- examples/text-editor/text-editor.v
+++ examples/text-editor/text-editor.v
@@ -1,6 +1,7 @@
 import malisipi.vwebui as webui
 import encoding.base64
 import os
+import json
 
 fn close(e &webui.Event) webui.Response {
 	webui.exit()
@@ -8,21 +9,36 @@ fn close(e &webui.Event) webui.Response {
 }
 
 fn open(e &webui.Event) webui.Response {
-	file := os.input("input file:")
+	if e.data.string == "" {
+		e.window.run("webui_fn('Open', prompt`File Location`)")
+		return 0
+	} else if e.data.string == "null" {
+		return 0
+	}
+	file := e.data.string
 	if file != "" {
-		file_content :=os.read_file(file) or {""}
+		file_content :=os.read_file(file) or { println("Failed to read file: ${file}") ""}
 
 		encoded_file := base64.encode_str(file)
 		encoded_file_content := base64.encode_str(file_content)
 		e.window.run("SetFile`${encoded_file}`")
 		e.window.run("addText`${encoded_file_content}`")
+		e.window.run("window.opened_file = `${file}`")
 	}
 	return 0
 }
 
+struct Save {
+	file string
+	content string
+}
+
 fn save(e &webui.Event) webui.Response {
-	content := e.data.string
-	println(content)
+	resp := json.decode(Save, e.data.string) or {
+		e.window.run("webui_fn('Save', JSON.stringify({file:window.opened_file,content:window.atob`${base64.encode_str(e.data.string)}`}))")
+		return 0
+	}
+	os.write_file(resp.file, resp.content) or { panic(err) }
 	return 0
 }
 
