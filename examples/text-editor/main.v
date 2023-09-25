bc42ce10d3ad2f661053c4f58ff6aea0b3a1bde0
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-09-26 01:06:43 +0200

    Update text-editor example

--- examples/text-editor/main.v
+++ examples/text-editor/main.v
@@ -1,53 +1,22 @@
 import vwebui as ui
 import os
 
-[heap]
-struct File {
-mut:
-	path string
-}
-
-fn (mut f File) open(e &ui.Event) {
-	println('Open')
-	file := e.get_arg[string]() or {
-		e.window.run("webui.call('Open', prompt`File Location`)")
-		return
-	}
-	file_content := os.read_file(file) or {
-		eprintln('Failed to read file: ${file}')
-		return
-	}
-	f_name := file.all_after_last(os.path_separator)
-	e.window.run('SetFileModeExtension`${os.file_ext(f_name)}`')
-	e.window.run('addText`${ui.encode(file_content)}`')
-	e.window.run('changeWindowTitle`${f_name}`')
-	f.path = file
-}
-
-fn (f &File) save(e &ui.Event) {
-	println('Save')
-	content := e.get_arg[string]() or { return }
-	os.write_file(f.path, content) or {
-		eprintln(err)
-		return
-	}
-}
-
 fn close(e &ui.Event) {
-	println('Close')
+	println('Exit.')
 	ui.exit()
 }
 
 fn main() {
 	w := ui.new_window()
 
-	w.set_root_folder(@VMODROOT)
+	w.bind[voidptr]('__close-btn', close)
 
-	mut file := File{}
-	w.bind[voidptr]('Open', file.open)
-	w.bind[voidptr]('Save', file.save)
-	w.bind[voidptr]('Close', close)
-	w.show('ui/index.html') or { panic(err) }
+	w.set_root_folder(os.join_path(@VMODROOT, 'ui'))
+
+	w.show_browser('index.html', .chromium_based) or {
+		w.show('index.html')!
+		eprintln('Warning: Install a Chromium-based web browser for an optimized experience.')
+	}
 
 	ui.wait()
 }
