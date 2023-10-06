a909e645ce973caefd82badb3f5fe6899b2732d8
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-06 11:44:22 +0200

    test: add test for returning values from V to JS

--- tests/call_v_from_js_test.v
+++ tests/call_v_from_js_test.v
@@ -87,17 +87,20 @@ fn test_get_js_arg() {
 	}
 }
 
+// V function bound to JS callback in next tests
+fn assert_get_args_at_idx(e &ui.Event) {
+	assert e.get_arg[string](idx: 0) or { '' } == 'foo'
+	assert e.get_arg[int](idx: 1) or { 0 } == 123
+	assert e.get_arg[bool](idx: 2) or { false } == true
+	assert e.get_arg[Person](idx: 3) or { Person{} } == Person{'Bob', 30}
+	assert e.get_arg[[]int](idx: 4) or { [0] } == [1, 2, 3]
+	e.window.close()
+}
+
 fn test_get_js_arg_at_idx() {
 	w := ui.new_window()
 	// V function, called from JS, receiving the above return value as argument and asserts it's correctness.
-	w.bind[voidptr]('v_fn_getting_arg_at_idx', fn [w] (e &ui.Event) {
-		assert e.get_arg[string](idx: 0) or { '' } == 'foo'
-		assert e.get_arg[int](idx: 1) or { 0 } == 123
-		assert e.get_arg[bool](idx: 2) or { false } == true
-		assert e.get_arg[Person](idx: 3) or { Person{} } == Person{'Bob', 30}
-		assert e.get_arg[[]int](idx: 4) or { [0] } == [1, 2, 3]
-		e.window.close()
-	})
+	w.bind[voidptr]('assert_get_args_at_idx', assert_get_args_at_idx)
 
 	script := '
 	setTimeout(async () => {
@@ -106,10 +109,53 @@ fn test_get_js_arg_at_idx() {
 			bool = true,
 			struct = { name: "Bob", age: 30 },
 			array = [1, 2, 3];
-		await webui.call("v_fn_getting_arg_at_idx", str, int, bool, JSON.stringify(struct), JSON.stringify(array));
+		await webui.call("assert_get_args_at_idx", str, int, bool, JSON.stringify(struct), JSON.stringify(array));
 	}, 500)'
 
 	// Show window, wait for it to be recognized as shown.
+	w.show(utils.gen_html(@FN, script),
+		await: true
+	) or { assert false, err.str() }
+
+	if !utils.timeout(60, fn [w] () bool {
+		return !w.is_shown()
+	}) {
+		assert false, 'Timeout while waiting JS calling V.'
+	}
+}
+
+fn test_return_value_to_js() {
+	w := ui.new_window()
+	// Eventually possible with lambda expressions like below. Re-check as V development progresses.
+	// w.bind('return_string', |_| 'bar')
+	w.bind('v_return_string', fn (e &ui.Event) string {
+		return 'foo'
+	})
+	w.bind('v_return_int', fn (e &ui.Event) int {
+		return 123
+	})
+	w.bind('v_return_bool', fn (e &ui.Event) bool {
+		return true
+	})
+	w.bind('v_return_struct', fn (e &ui.Event) Person {
+		return Person{'Bob', 30}
+	})
+	w.bind('v_return_array', fn (e &ui.Event) []int {
+		return [1, 2, 3]
+	})
+	// V function, called from JS, receiving the above return value as argument and asserts it's correctness.
+	w.bind[voidptr]('assert_js_res_from_v', assert_get_args_at_idx)
+
+	script := '
+	setTimeout(async () => {
+		const str = await webui.call("v_return_string"),
+			int = await webui.call("v_return_int"),
+			bool = await webui.call("v_return_bool"),
+			struct = await webui.call("v_return_struct"),
+			array = await webui.call("v_return_array");
+			console.log(struct)
+		await webui.call("assert_js_res_from_v", str, int, bool, struct, array);
+	}, 500)'
 	w.show(utils.gen_html(@FN, script),
 		await: true
 	) or { assert false, err.str() }
